a07d0fb3ee61983e0fd0b6ca3f94a0257a70abe2
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2015-08-29 13:19:32 -0300

    Removido estrutura de execução com transação para super classe

---
+++ planner-jpa/src/main/scala/com/codesimples/jpa/JPATemplateBuilder.scala
@@ -0,0 +1,24 @@
+package com.codesimples.jpa
+
+import javax.persistence.EntityManager
+
+import com.typesafe.scalalogging.Logger
+
+trait JPATemplateBuilder {
+  implicit val logger: Logger
+  implicit val entityManager: EntityManager
+
+  def withTransaction( callback:  => Unit ) = {
+    val transaction = entityManager.getTransaction
+    transaction.begin()
+    try {
+      callback
+      transaction.commit()
+    } catch {
+      case e: Exception => {
+        if(transaction.isActive) transaction.rollback()
+        logger.error("Transaction Error: ", e)
+      }
+    }
+  }
+}
--- planner-jpa/src/main/scala/com/codesimples/jpa/adapter/user/NewUserPersistenceAdapterJPA.scala
+++ planner-jpa/src/main/scala/com/codesimples/jpa/adapter/user/NewUserPersistenceAdapterJPA.scala
@@ -1,5 +1,6 @@
 package com.codesimples.jpa.adapter.user
 
+import com.codesimples.jpa.JPATemplateBuilder
 import com.codesimples.jpa.domain.User
 import com.codesimples.jpa.repositories.UserRepository
 import com.codesimples.objectives.persistence.adapter.user.NewUserPersistenceAdapter
@@ -8,22 +9,9 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConversions._
 
-class NewUserPersistenceAdapterJPA(val userRepository: UserRepository) extends NewUserPersistenceAdapter {
+class NewUserPersistenceAdapterJPA(val userRepository: UserRepository) extends JPATemplateBuilder with NewUserPersistenceAdapter {
   val logger = Logger( LoggerFactory.getLogger( this.getClass ) )
-
-  def withTransaction( callback:  => Unit ) = {
-    val transaction = userRepository.entityManager.getTransaction
-    transaction.begin()
-    try {
-      callback
-      transaction.commit()
-    } catch {
-      case e: Exception => {
-        if(transaction.isActive) transaction.rollback()
-        logger.error("Transaction Error: ", e)
-      }
-    }
-  }
+  val entityManager = userRepository.entityManager
 
   override def saveUser(map: Map[String, AnyRef]) = {
     def user = new User(map)
